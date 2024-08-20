aebc07dbe51a836d933b302e2a6511d799008f17
Alo Odunayo Oluwaseun <laurie143pelumi@gmail.com>
2024-08-20 15:41:59 +0100

    feat: caching

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const port = process.env.PORT||3000;
 // Your API endpoint
 const apiEndpoint = 'https://api.skyskillhub.com/wake';
 const apiEndpoint2 = 'https://binary-bloggy.onrender.com/';
+const apiEndpoint3 = 'https://wake-up-server-8pcm.onrender.com';
+
 // const apiEndpoint = 'http://localhost:4000/wake';
 
 // Define the cron schedule (every two minutes between 5am to 1am)
@@ -20,10 +22,12 @@ async function makeApiRequest() {
   try {
     const response = await axios.get(apiEndpoint);
     const response2 = await axios.get(apiEndpoint2);
+    const response3 = await axios.get(apiEndpoint3);
     
     // Process the API response data if needed
     console.log(response.data);
     console.log("Response to blog",response2.status);
+    console.log("Response to render 2 wakeup",response2.data);
   } catch (error) {
     console.error('Error during API request:', error.message);
   }
